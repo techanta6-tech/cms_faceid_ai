9ac5f9fa67fe85a3d27fe04dbcc9ae60bdc5ff05
phucphan01866 <111278365+phucphan01866@users.noreply.github.com>
2026-07-14 10:31:58 +0700

    Update init files

--- apps/backend/src/prisma/cms-prisma.service.ts
+++ apps/backend/src/prisma/cms-prisma.service.ts
@@ -3,6 +3,16 @@ import { PrismaClient } from '../../generated/cms_webserver';
 
 @Injectable()
 export class CmsPrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: process.env.CMS_WEBSERVER_DATABASE_URL || 'postgresql://postgres:@localhost:5433/cms_webserver?connect_timeout=15',
+        },
+      },
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
--- apps/backend/src/prisma/lcms-prisma.service.ts
+++ apps/backend/src/prisma/lcms-prisma.service.ts
@@ -3,6 +3,16 @@ import { PrismaClient } from '../../generated/lcms';
 
 @Injectable()
 export class LcmsPrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: process.env.LCMS_DATABASE_URL || 'postgresql://postgres:@localhost:5433/lcms_server?connect_timeout=15&pooling=true&minpoolsize=0&maxpoolsize=200',
+        },
+      },
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
